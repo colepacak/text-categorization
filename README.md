e3a4d395c34bc472c388a2a458527ac5e07a7f3d
colepacak <cole@favish.com>
2019-03-07 07:51:35 -0600

    show most common words by category

--- README.md
+++ README.md
@@ -13,5 +13,18 @@ First, let's see a breakdown of the category counts throughout the whole dataset
 
 ![Category Counts for BBC Articles](src/figures/category-counts.png)
 
+Next, I'm curious what the most commonly used words are in each category. Right now, I'm just going to use unigrams when tokenizing, but I'm wondering if seeing the most common words might point out the need to use bigrams. Also, it would be interesting to know whether tf-idf may increase accuracy by downscaling the importance of words that appear too often to be useful.
+
+### Most Common Words by category
+![Most common words - business](src/figures/most-common-word-counts-business.png)
+![Most common words - entertainment](src/figures/most-common-word-counts-entertainment.png)
+![Most common words - politics](src/figures/most-common-word-counts-politics.png)
+![Most common words - sport](src/figures/most-common-word-counts-sport.png)
+![Most common words - tech](src/figures/most-common-word-counts-tech.png)
+
+## TODO
+* See how adding a tf-idf transformer to a sklearn pipeline affects accuracy.
+* Look at the effect of tf-idf on the top 5 words in each category.
+
 ## Local Development
 Run `docker-compose up` in the project root to launch a run a Python container that includes all of the dependencies, which can be found in `.docker/requirements.txt`. I downloaded the Spacy model in the dockerfile so that I didn't need to do it manually later on.
--- src/text-cat-sklearn.py
+++ src/text-cat-sklearn.py
@@ -1,6 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import spacy
+import string
+from nltk.corpus import stopwords
+from collections import Counter
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+
+nlp = spacy.load('en_core_web_sm')
+stopwords = stopwords.words('english')
+punctuations = string.punctuation
+STOPLIST = set(stopwords + list(ENGLISH_STOP_WORDS))
+SYMBOLS = " ".join(string.punctuation).split(" ") + ["-", "...", "”", "”"]
 
 # Load the data.
 df = pd.read_csv('data/bbc-text.csv')
@@ -8,4 +19,36 @@ df = pd.read_csv('data/bbc-text.csv')
 fig = plt.figure(figsize=(8,4))
 sns.barplot(x = df['category'].unique(), y = df['category'].value_counts())
 plt.title('Category Counts for BBC Articles')
-plt.savefig('figures/category-counts')
+# plt.savefig('figures/category-counts')
+
+# Get most common words by category.
+def clean_text(docs):
+    texts = []
+    for doc in docs:
+        doc = nlp(doc, disable=['parser', 'ner'])
+        tokens = [tok.lemma_.lower().strip() for tok in doc if tok.lemma_ != '-PRON-']
+        tokens = [tok for tok in tokens if tok not in STOPLIST]
+        tokens = [tok for tok in tokens if tok not in SYMBOLS]
+        tokens = ' '.join(tokens)
+        texts.append(tokens)
+
+    return texts
+
+def get_most_common_word_counts_by_category():
+    for category in df['category'].unique():
+        # Get list of articles filtered by category.
+        texts = [text for text in df[df['category'] == category]['text']]
+        # Get a list cleaned articles.
+        texts_clean = clean_text(texts)
+        # Get a list of all tokens in the category.
+        tokens = ' '.join(texts_clean).split()
+        token_counts = Counter(tokens)
+        common_words = [word[0] for word in token_counts.most_common(20)]
+        common_counts = [word[1] for word in token_counts.most_common(20)]
+
+        fig = plt.figure(figsize=(18,6))
+        sns.barplot(x = common_words, y = common_counts)
+        plt.title('BBC Articles - Most Common Words - ' + category)
+        plt.savefig('figures/most-common-word-counts-' + category)
+
+get_most_common_word_counts_by_category()
